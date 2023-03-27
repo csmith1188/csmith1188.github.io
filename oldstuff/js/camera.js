c9064a0e71f699d1398db59acc99cb2c2e540099
Mr. Smith <csmith@ytech.edu>
2023-03-27 14:52:23 -0400

    up

--- oldstuff/js/camera.js
+++
@@ -1,9 +0,0 @@
-class Camera {
-    constructor(options) {
-        this.target;
-        if (typeof options === 'object')
-            for (var key of Object.keys(options)) {
-                this[key] = options[key];
-            }
-    }
-}
