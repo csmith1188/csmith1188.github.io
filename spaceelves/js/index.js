e2dc53a3731840ba1a1e53feecf2ab8cde8fa714
Mr. Smith <csmith@ytech.edu>
2024-09-28 08:21:03 -0400

    Delta Time Update

--- spaceelves/js/index.js
+++ spaceelves/js/index.js
@@ -6,21 +6,19 @@ let allID = 0;
 
 window.onload = function () {
     canvas = document.getElementById("game-canvas");
+    scale_canvas = document.getElementById("scale-canvas");
     ctx = canvas.getContext("2d");
+    sctx = scale_canvas.getContext("2d");
     ctx.imageSmoothingEnabled = false;
-
+    sctx.imageSmoothingEnabled = false;
+    
     game = new Game();
 
-    // game.match = new DebugMatch();
     game.match = new Start_Screen();
+    // game.match = new DebugMatch();
     // game.match = new Match_ForEver();
-    // game.match = new Match_ForHonor();
+    // game.match = new Match_OnlineMP();
 
     //Run the step() function every 16ms (60fps)
     setInterval(game.step.bind(game), 16);
 }
-
-
-
-
-
